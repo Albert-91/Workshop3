3106b06651f41655016e7239d70cfa253f3a96b1
Tomasz Staszkiewicz <tomsta.kreacja@gmail.com>
2018-06-23 13:35:46 +0200

    puste widoki

--- workshop/rooms_reservation/views.py
+++ workshop/rooms_reservation/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.views import View
+from django.utils.decorators import method_decorator
+from datetime import datetime, timedelta
+from rooms_reservation.models import *
 # Create your views here.
+
+
+
+@csrf_exempt
+def show_rooms(request):
+    pass
+
+
+@csrf_exempt
+def details_room(request):
+    pass
+
+@csrf_exempt
+def edit_room(request):
+    pass
+
+@csrf_exempt
+def add_room(request):
+    pass
+
+@csrf_exempt
+def search_room(request):
+    pass
+
+@csrf_exempt
+def delete_room(request):
+    pass
--- workshop/workshop/urls.py
+++ workshop/workshop/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import url
+from rooms_reservation.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
